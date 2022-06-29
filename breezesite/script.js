337cbe817367cf9448b3716feda1ff84b47b7f7f
BreezeIV <108148674+BreezeIV@users.noreply.github.com>
2022-06-29 10:13:32 +0200

    Update Breeze

--- breezesite/script.js
+++ breezesite/script.js
@@ -1,7 +1,8 @@
+var a = 0
 const mousevent = document.querySelector('.mousevent')
 const vertical = document.querySelector('.vertical')
 const horizontal = document.querySelector('.horizontal')
-mousevent.addEventListener('mousemove', (event) => {
+mousevent.addEventListener('click', (event) => {
     horizontal.innerHTML = event.x
     vertical.innerHTML = event.y
 })
@@ -10,5 +11,20 @@ mousevent.addEventListener('click', () =>{
   btn.classList.toggle('.montrer');
   console.log('tout a fait');
 })
+
+const click =( () =>{
+  console.log('et la peut être ?');
+})
+
+
+const btn = document.querySelector('.btn')
+const btnnum = document.querySelector('.btnnum')
+
+btn.addEventListener('click', () =>{
+  a++
+  btnnum.innerHTML = a
+  console.log('la c fait mdrrr');
+})
+
 
 //regarder : div, listes en html, span
